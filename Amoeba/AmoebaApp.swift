f5ef7936f45b396a4d4cef2ee7716a07406abd57
Ivan Putra Eriansya <eriansha.van@gmail.com>
2023-03-20 12:55:38 +0700

    Initial Commit

---
+++ Amoeba/AmoebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmoebaApp.swift
+//  Amoeba
+//
+//  Created by Ivan on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AmoebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
